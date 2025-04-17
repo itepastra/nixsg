2c5d792c0e640ce87cd0305eba5a5d18ea5bfd75
Noa Aarts <noa@voorwaarts.nl>
2025-04-17 12:05:09 +0200

    add some basic testing

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
   outputs =
-    { nixpkgs }:
+    {
+      nixpkgs,
+      self,
+    }:
     let
       inherit (nixpkgs) lib;
       systems = lib.intersectLists lib.systems.flakeExposed lib.platforms.linux;
@@ -23,9 +26,33 @@
       checks = forAllSystems (
         system:
         let
-          lib = import lib { inherit system; };
+          nlib = lib.${system};
+          pkgs = nixpkgsFor.${system};
         in
         {
+          commonmark =
+            let
+              results = pkgs.lib.runTests (import ./test.nix { inherit nlib pkgs; });
+              formatValue =
+                val:
+                if (builtins.isList val || builtins.isAttrs val) then
+                  builtins.toJSON val
+                else
+                  builtins.toString val;
+              resultToString =
+                {
+                  name,
+                  expected,
+                  result,
+                }:
+                ''
+                  ${name} failed: expected ${formatValue expected}, but got ${formatValue result}
+                '';
+            in
+            if results != [ ] then
+              builtins.throw (builtins.concatStringsSep "\n" (map resultToString results))
+            else
+              pkgs.runCommand "nix-flake-tests-success" { } "echo > $out";
         }
       );
     };
--- lib.nix
+++ lib.nix
@@ -4,62 +4,52 @@
 let
   lib = pkgs.lib;
 in
-{
+rec {
+  # Parse a markdown header to a typed content
+  header =
+    line:
+    (lib.findFirst (lib.getAttr "cond") { } (
+      lib.attrsets.mapAttrsToList
+        (str: type: {
+          cond = lib.hasPrefix str line;
+          out = {
+            type = type;
+            content = lib.trim (lib.substring (lib.stringLength str) (lib.stringLength line) line);
+          };
+        })
+        {
+          "# " = "h1";
+          "## " = "h2";
+          "### " = "h3";
+          "#### " = "h4";
+          "##### " = "h5";
+          "###### " = "h6";
+        }
+    )).out or {
+      type = "p";
+      content = line;
+    };
   # Markdown to HTML
   markdownToHTML =
     markdownString:
     let
       lines = lib.splitString "\n" markdownString;
-      blocks =
-        let
-          header =
-            line:
-            if lib.hasPrefix "# " line then
-              {
-                content = lib.substring (lib.stringLength "# ") (lib.stringLength line) line;
-                type = "h1";
-              }
-            else if lib.hasPrefix "## " line then
-              {
-                content = lib.substring (lib.stringLength "## ") (lib.stringLength line) line;
-                type = "h2";
-              }
-            else if lib.hasPrefix "### " line then
-              {
-                content = lib.substring (lib.stringLength "### ") (lib.stringLength line) line;
-                type = "h3";
-              }
-            else if lib.hasPrefix "#### " line then
-              {
-                content = lib.substring (lib.stringLength "#### ") (lib.stringLength line) line;
-                type = "h4";
-              }
-            else if lib.hasPrefix "##### " line then
-              {
-                content = lib.substring (lib.stringLength "##### ") (lib.stringLength line) line;
-                type = "h5";
-              }
-            else if lib.hasPrefix "###### " line then
-              {
-                content = lib.substring (lib.stringLength "###### ") (lib.stringLength line) line;
-                type = "h6";
-              }
-            else
-              [ ];
-        in
-        builtins.foldl' (
-          pblocks: line:
-          pblocks
-          ++ (
-            if lib.hasPrefix "#" line then
-              header line
-            else if lib.match "^\s{,3}([*\-=_])(\1\s*){2,}$" line then
-              { type = "hr"; }
-            # TODO: all but (thematic) headings
-            else
-              { }
-          )
-        ) [ ] lines;
+      blocks = builtins.foldl' (
+        pblocks: line:
+        pblocks
+        ++ (
+          if lib.hasPrefix "#" line then
+            header line
+          else if lib.match "\s{,3}([\*\-=_])(\1\s*){2,}" line then
+            { type = "hr"; }
+          # TODO: all but (thematic) headings
+          else
+            {
+              type = "p";
+              content = line;
+            }
+        )
+      ) [ ] lines;
     in
     { };
 }
